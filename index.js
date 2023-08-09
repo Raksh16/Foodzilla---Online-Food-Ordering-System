597fab64309a9345697d4f36b7c298eb73cb8c5a
Raksh16 <107063580+Raksh16@users.noreply.github.com>
2023-08-09 17:28:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var ejs = require("ejs");
+
+var app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.listen(8000);
+
+//localhost:8000
+app.get("/", function (req, res) {
+  res.render("pages/index");
+});
